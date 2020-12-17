c939ca07dabd7604da81436689bf00fe7e155247
Yura-Msscs <73899279+Yura-Msscs@users.noreply.github.com>
2020-12-17 09:15:37 +0200

    Add files via upload

--- morboy.py
+++ morboy.py
@@ -4,10 +4,26 @@ tk=Tk()
 canvas=Canvas(tk,width=500,height=500)
 canvas.pack()
 ship_image=PhotoImage(file='ship.gif')
-s=canvas.create_image(500,0,anchor=NW,image=ship_image)
 canvas.create_rectangle(190,480,310,500,fill='black')
 canvas.create_rectangle(240,460,260,480,fill='black')
-for y in range (200):
-    canvas.move(s,-3,0)
-    tk.update()
-    time.sleep(0.02)
+shots=0
+canvas.create_text(420,100,text="кількість пострілів"+str(shots))
+b=0
+def ball(event):
+    global b,shots
+    if shots<10:
+        canvas.delete(b)
+        b=canvas.create_oval(240,460,260,480,fill='gray')
+        canvas.create_text(420,100,text="кількість пострілів"+str(shots),fill='#F0F0ED')
+        shots=shots+1
+        canvas.create_text(420,100,text="кількість пострілів"+str(shots))
+    else:
+        canvas.create_text(50,100,text='Ядра закінчились')
+canvas.bind_all('<space>',ball)
+for z in range(10):
+    s=canvas.create_image(500,0,anchor=NW,image=ship_image)
+    for y in range (200):
+        canvas.move(s,-3,0)
+        canvas.move(b,0,-5)
+        tk.update()
+        time.sleep(0.02)
